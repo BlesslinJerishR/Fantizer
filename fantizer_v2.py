eb5c6f332ea37815997042ef7370e59eaa920357
Mastermindx33 <mastermindx33@gmail.com>
2021-11-16 08:05:52 +0530

    Update fantizer_v2.py

--- fantizer_v2.py
+++ fantizer_v2.py
@@ -1,2 +1,22 @@
-def add_inventory(inv, item):
-    forx in inv:
+def add_inventory(inv, loot):
+    for x, y in inv.items():
+        for z in loot:
+            inv[z] = y
+            break
+        return inv
+
+
+def inventory(dic):
+    total = 0
+    for d, i in dic.items():
+        total += i
+        print(i, d)
+    print(f"Total number of items in inventory were : {total}")
+
+
+inv = {'gold': 42, 'haste': 3}
+loot = ['gold', 'elixer', 'gems']
+
+inn = add_inventory(inv, loot)
+print(inn)
+inventory(inn)
